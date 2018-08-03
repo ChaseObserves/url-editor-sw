1ca1c36789e2e46dc3262cf4ed9d8b7552859d43
Chase Adams <chaseme37@gmail.com>
2018-08-03 09:02:02 -0600

    latest commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@
 var fs = require("fs");
 var express = require("express");
 
-var data = fs.readFileSync("all-blog-posts.json");
+var data = fs.readFileSync("all-blog-posts-CURRENT.json");
 var blogs = JSON.parse(data);
 
-var moreData = fs.readFileSync("full-recipes.json");
+var moreData = fs.readFileSync("all-recipes-CURRENT.json");
 var recipes = JSON.parse(moreData);
 
 // SET UP SERVER
@@ -38,8 +38,35 @@ function addURLblogs() {
         .replace(/[\"][/]uploads/g, "https://www.sunwarrior.com/uploads")
         .replace(/[\\]/g, "")
         .replace(/rnrn/g, "")
-        // .replace(/[\"]/g, "")
-        .replace(/>rn/g, ">");
+        .replace(/>rn/g, ">")
+        .replace(/[\"][/]store[/]product[/]warrior-.*?[\"]/g, "\"https://www.sunwarrior.com/collections/all/products/warrior-blend\"")
+        .replace(/com[/]store[/]product[/]warrior-.*?[\"]/g, "com/collections/all/products/warrior-blend\"")
+        .replace(/[\"][/]store[/]product[/]classic-protein.*?[\"]/g, "\"https://www.sunwarrior.com/collections/all/products/classic-protein\"")
+        .replace(/com[/]store[/]product[/]classic-protein.*?[\"]/g, "com/collections/all/products/classic-protein\"")
+        .replace(/[\"][/]store[/]product[/]classic-plus.*?[\"]/g, "\"https://www.sunwarrior.com/collections/all/products/classic-plus\"")
+        .replace(/com[/]store[/]product[/]classic-plus.*?[\"]/g, "com/collections/all/products/classic-plus\"")
+        .replace(/com[/]store[/]product[/]illumin8.*?[\"]/g, "com/collections/all/products/illumin8\"")
+        .replace(/[\"][/]store[/]product[/]liquid-light.*?[\"]/g, "\"https://www.sunwarrior.com/collections/all/products/us-liquid-light-946-4-ml-32-fl-oz-bottle\"")
+        .replace(/com[/]store[/]product[/]liquid-light.*?[\"]/g, "com/collections/all/products/us-liquid-light-946-4-ml-32-fl-oz-bottle\"")
+        .replace(/[\"][/]store[/]product[/]ormus-.*?[\"]/g, "\"https://www.sunwarrior.com/collections/all/products/ormus-super-greens\"")
+        .replace(/com[/]store[/]product[/]ormus-.*?[\"]/g, "com/collections/all/products/ormus-super-greens\"")
+        .replace(/[\"][/]store[/]product[/]vitamin-mineral-rush.*?[\"]/g, "\"https://www.sunwarrior.com/collections/all/products/us-vmr-236-5-ml-8-fl-oz\"")
+        .replace(/com[/]store[/]product[/]vitamin-mineral-rush.*?[\"]/g, "com/collections/all/products/us-vmr-236-5-ml-8-fl-oz\"")
+        .replace(/[\"][/]store[/]product[/]vitamins-for-her.*?[\"]/g, "\"https://www.sunwarrior.com/collections/all/products/us-raw-vitamins-for-her-90-capsules-bottle\"")
+        .replace(/com[/]store[/]product[/]vitamins-for-her.*?[\"]/g, "com/collections/all/products/us-raw-vitamins-for-her-90-capsules-bottle\"")
+        .replace(/[\"][/]store[/]product[/]vitamins-for-him.*?[\"]/g, "\"https://www.sunwarrior.com/collections/all/products/us-raw-vitamins-for-him-90-capsules-bottle\"")
+        .replace(/com[/]store[/]product[/]vitamins-for-him.*?[\"]/g, "com/collections/all/products/us-raw-vitamins-for-him-90-capsules-bottle\"")
+        .replace(/[\"][/]store[/]product[/]immune-shield.*?[\"]/g, "\"https://www.sunwarrior.com/collections/all/products/us-immune-shield-236-5-ml-8-fl-oz-bottle\"")
+        .replace(/com[/]store[/]product[/]immune-shield.*?[\"]/g, "com/collections/all/products/us-immune-shield-236-5-ml-8-fl-oz-bottle\"")
+        .replace(/com[/]store[/]product[/]cplus-.*?[\"]/g, "com/collections/all/products/classic-plus\"")
+        .replace(/com[/]store[/]product[/]sol-.*?[\"]/g, "com/collections/all/products/sol-good-protein-bars\"")
+        .replace(/com[/]store[/]product[/]wb-.*?[\"]/g, "com/collections/all/products/warrior-blend\"")
+        .replace(/com[/]store[/]product[/]enzorb.*?[\"]/g, "com/collections/all/products/us-enzorb-90-count-capsules-bottle\"")
+        .replace(/com[/]store[/]product[/]classic-.*?-sm[\"]/g, "com/collections/all/products/classic-protein\"")
+        .replace(/com[/]store[/]product[/]eco-.*?[\"]/g, "com/collections/all/products/ecovessel-boulder-24-oz\"")
+        .replace(/com[/]healthhub[/]/g, "com/blogs/health-hub/")
+        .replace(/[\"][/]healthhub[/]/g, "\"https://www.sunwarrior.com/blogs/health-hub/")
+        .replace(/=https:/g, "=\"https:");
       blogs[key].articleBodyHTML = finalOutput;
     }
   }
@@ -65,25 +92,31 @@ function addURLrecipes() {
         directionsHTML.slice(directionsPosition)
       ].join("");
       finalOutput = output
-        .replace(/[\"][/]uploads/g, "https://www.sunwarrior.com/uploads")
+        .replace(/[\"][/]uploads/g, "\"https://www.sunwarrior.com/uploads")
         .replace(/[\\]/g, "")
         .replace(/rnrn/g, "")
-        .replace(/>rn/g, ">");
+        .replace(/>rn/g, ">")
+        .replace(/com[/]healthhub[/]/g, "com/blogs/health-hub/")
+        .replace(/[\"][/]healthhub[/]/g, "\"https://www.sunwarrior.com/blogs/health-hub/");
       recipes[key].articleBodyHTML = finalOutput;
       finalIngredients = ingredientsHTML
-        .replace(/[\"][/]uploads/g, "https://www.sunwarrior.com/uploads")
+        .replace(/[\"][/]uploads/g, "\"https://www.sunwarrior.com/uploads")
         .replace(/[\\]/g, "")
         .replace(/rnrn/g, "")
         .replace(/rnt/g, "")
         .replace(/>rn/g, ">")
-        .replace(/[\"]s:.*:[\"]/g, "");
+        .replace(/[\"]s:.*:[\"]/g, "")
+        .replace(/com[/]healthhub[/]/g, "com/blogs/health-hub/")
+        .replace(/[\"][/]healthhub[/]/g, "\"https://www.sunwarrior.com/blogs/health-hub/");
       recipes[key].ingredients = finalIngredients;
       finalDirections = directionsOutput
-        .replace(/[\"][/]uploads/g, "https://www.sunwarrior.com/uploads")
+        .replace(/[\"][/]uploads/g, "\"https://www.sunwarrior.com/uploads")
         .replace(/[\\]/g, "")
         .replace(/rnrn/g, "")
         .replace(/rnt/g, "")
-        .replace(/>rn/g, ">");
+        .replace(/>rn/g, ">")
+        .replace(/com[/]healthhub[/]/g, "com/blogs/health-hub/")
+        .replace(/[\"][/]healthhub[/]/g, "\"https://www.sunwarrior.com/blogs/health-hub/");
       recipes[key].directions = finalDirections;
     }
   }
@@ -94,8 +127,8 @@ function addURLrecipes() {
 function saveFile() {
   newBlogData = JSON.stringify(blogs, null, 2);
   newRecipeData = JSON.stringify(recipes, null, 2);
-  fs.writeFile("all-blog-posts-new.json", newBlogData, finished);
-  fs.writeFile("full-recipes-new.json", newRecipeData, finished);
+  fs.writeFile("all-blog-posts-CURRENT-new.json", newBlogData, finished);
+  fs.writeFile("full-recipes-CURRENT-new.json", newRecipeData, finished);
   function finished(err) {
     console.log("all set!");
   }
